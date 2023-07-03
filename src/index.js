cefc77ab55d66af029acfc40c9f3d278c1f2bf1a
matt94nanari <wind.traveler@hotmail.com>
2023-07-03 15:50:32 +0800

    add service work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.scss'
+// import App from './App'
+// import reportWebVitals from './reportWebVitals'
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+// reportWebVitals()
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 import './i18n'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
+serviceWorkerRegistration.register()
 reportWebVitals()
